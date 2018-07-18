d712805471dc2dcf5463f7e7bbf8371bee8b0be0
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-07-18 15:06:01 +0100

    start up at menu screen

--- main.lua
+++ main.lua
@@ -3,3 +3,15 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
+
+-- load composer to manage scenes
+local composer = require("composer")
+
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- seed random number generator
+math.randomseed(os.time())
+
+-- start off at menu screen
+composer.gotoScene("menu")
