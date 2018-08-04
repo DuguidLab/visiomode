a92daec6e1fbc4360cfeb97cbec11cb71c7b63cc
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-08-04 20:32:43 +0100

    switch to socketserver instead of raw socket

--- bin/listener.py
+++ bin/listener.py
@@ -1,39 +1,30 @@
-import socket
-import threading
+import socketserver
 
 
-BIND_IP = '0.0.0.0'
-BIND_PORT = 5050
+class TCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
 
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
 
-def listen():
-    """listener loop"""
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((BIND_IP, BIND_PORT))
-    server.listen(5)  # max connections
-
-    print("Listening on {}:{}".format(BIND_IP, BIND_PORT))
-
-    def client_connection(client_socket):
-        request = client_socket.recv(1024)
-        print("Received {}".format(request))
-
-        # Some processing here
-
-        return client_socket.close()
-
-    while True:
-        client_sock, address = server.accept()
-        print("Connection from {}:{}".format(address[0], address[1]))
-        client_handle = threading.Thread(
-                target=client_connection,
-                args=(client_sock,)
-            )
-        client_handle.start()
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
 
 
 if __name__ == '__main__':
-    try:
-        listen()
-    except KeyboardInterrupt:
-        exit()
+    BIND_IP = '0.0.0.0'
+    BIND_PORT = 5050
+
+    with socketserver.TCPServer((BIND_IP, BIND_PORT), TCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
+
