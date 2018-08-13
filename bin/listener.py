51dedb9278b03d93fc0fca38d645203b76064e86
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-08-13 16:53:21 +0100

    handle TCP connection until it is shut by the client

--- bin/listener.py
+++ bin/listener.py
@@ -1,5 +1,6 @@
 import socketserver
-import rodent_control.core as rc
+#import rodent_control.core as rc
+import time
 
 
 class TCPHandler(socketserver.BaseRequestHandler):
@@ -10,22 +11,28 @@ class TCPHandler(socketserver.BaseRequestHandler):
     override the handle() method to implement communication to the
     client.
     """
-
     def handle(self):
         # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        if str(self.data) == 'test':
-            rc.water_reward(delay=1000)
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        while True:
+            self.data = self.request.recv(1024)  # Only suitable for single clients!
+            if not self.data:
+                break
+            if str(self.data, 'utf-8') == 'connect':
+                print('thing')
+                self.request.sendall(b'test\n')
+                time.sleep(1)
+                self.request.sendall(b'{"something": "a", "thing": "b"}\n')
+            if self.data == b'test':
+                print('test water reward')
+                #rc.water_reward(delay=1000)
+            print("{}:{} wrote: {}".format(self.client_address[0], self.client_address[1], str(self.data, 'utf-8')))
 
 
 if __name__ == '__main__':
     BIND_IP = '0.0.0.0'
-    BIND_PORT = 5050
+    BIND_PORT = 5000
 
+    socketserver.TCPServer.allow_reuse_address = True
     with socketserver.TCPServer((BIND_IP, BIND_PORT), TCPHandler) as server:
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
