33e2c1bbee49231d0a8249337c46335aa96632ed
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-08-13 18:48:12 +0100

    prompt user for session settings, wrap all settings in one call

--- src/rodent_control/user/cli_prompts.py
+++ src/rodent_control/user/cli_prompts.py
@@ -2,6 +2,16 @@
 import json
 
 
+def all_settings(as_json=True):
+    all_settings = {
+            'task': task_settings(as_json=False),
+            'session': session_settings(as_json=False)
+        }
+    if as_json:
+        return json.dumps(all_settings)
+    return all_settings
+
+
 def task_settings(as_json=True):
     task_settings = {
             'targets': input("Number of targets: "),
@@ -9,8 +19,8 @@ def task_settings(as_json=True):
             'distance': input("Distance between targets (mm): "),
             'delay': input("Presentation delay (ms): "),
             'haptics': False if input("Haptic feedback (Y/N): ") == 'N' else True,
-            'animated': True if input("Animated (Y/N): ") != 'Y' else False,
-            'shrinking': True if input("Shrinking (Y/N): ") != 'Y' else False
+            'animated': True if input("Animated (Y/N): ") == 'Y' else False,
+            'shrinking': True if input("Shrinking (Y/N): ") == 'Y' else False
         }
     if as_json:
         return json.dumps(task_settings)
@@ -18,7 +28,16 @@ def task_settings(as_json=True):
 
 
 def session_settings(as_json=True):
-    return
+    session_settings = {
+            'sessionType': 'rpi',
+            'duration': input("Session duration (min): "),
+            'showResults': False,
+            'playToneAtEnd': True if input("Tone at End (Y/N): ") == 'Y' else False,
+            'saveSession': True
+        }
+    if as_json:
+        return json.dumps(session_settings)
+    return session_settings
 
 
 def animation_settings(as_json=True):
