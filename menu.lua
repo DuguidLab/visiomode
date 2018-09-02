b7e99b1bcbb39d46d4be82cd1020baffbf5ce45e
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-09-02 21:06:24 +0100

    fix spacing issue (again)

--- menu.lua
+++ menu.lua
@@ -8,17 +8,17 @@ local scene = composer.newScene()
 -- -----------------------------------------------------------------------------------
 
 local function gotoConnectRpi()
-  composer.gotoScene("connect")
+    composer.gotoScene("connect")
 end
 
 
 local function gotoStandAlone()
-  composer.gotoScene("stand-alone-setup")
+    composer.gotoScene("stand-alone-setup")
 end
 
 
 local function gotoAbout()
-  composer.gotoScene("about")
+    composer.gotoScene("about")
 end
 
 
@@ -29,66 +29,66 @@ end
 -- create()
 function scene:create( event )
 
-  local sceneGroup = self.view
+    local sceneGroup = self.view
 
-  local background = display.newImageRect(sceneGroup, "assets/menu-background.jpg", 1400, 800)
-  background.x = display.contentCenterX
-  background.y = display.contentCenterY
+    local background = display.newImageRect(sceneGroup, "assets/menu-background.jpg", 1400, 800)
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
 
-  local title = display.newText(sceneGroup, "Rodent Touchscreen Behaviour Suite", display.contentCenterX, 50, native.systemFont, 60)
+    local title = display.newText(sceneGroup, "Rodent Touchscreen Behaviour Suite", display.contentCenterX, 50, native.systemFont, 60)
 
-  local connectButton = display.newText(sceneGroup, "Connect to RPi Controller", display.contentCenterX, 300, native.systemFont, 44)
-  connectButton:setFillColor(0.75, 0.78, 1)
+    local connectButton = display.newText(sceneGroup, "Connect to RPi Controller", display.contentCenterX, 300, native.systemFont, 44)
+    connectButton:setFillColor(0.75, 0.78, 1)
 
-  local standAloneButton = display.newText(sceneGroup, "Stand-Alone Mode", display.contentCenterX, 400, native.systemFont, 44)
-  standAloneButton:setFillColor(0.75, 0.78, 1)
+    local standAloneButton = display.newText(sceneGroup, "Stand-Alone Mode", display.contentCenterX, 400, native.systemFont, 44)
+    standAloneButton:setFillColor(0.75, 0.78, 1)
 
-  local aboutButton = display.newText(sceneGroup, "About", display.contentCenterX, 500, native.systemFont, 44)
-  aboutButton:setFillColor(0.75, 0.78, 1)
+    local aboutButton = display.newText(sceneGroup, "About", display.contentCenterX, 500, native.systemFont, 44)
+    aboutButton:setFillColor(0.75, 0.78, 1)
 
-  connectButton:addEventListener("tap", gotoConnectRpi)
-  standAloneButton:addEventListener("tap", gotoStandAlone)
-  aboutButton:addEventListener("tap", gotoAbout)
+    connectButton:addEventListener("tap", gotoConnectRpi)
+    standAloneButton:addEventListener("tap", gotoStandAlone)
+    aboutButton:addEventListener("tap", gotoAbout)
 end
 
 
 -- show()
 function scene:show( event )
 
-	local sceneGroup = self.view
-	local phase = event.phase
+    local sceneGroup = self.view
+    local phase = event.phase
 
-	if ( phase == "will" ) then
-		-- Code here runs when the scene is still off screen (but is about to come on screen)
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is still off screen (but is about to come on screen)
 
-	elseif ( phase == "did" ) then
-		-- Code here runs when the scene is entirely on screen
+    elseif ( phase == "did" ) then
+        -- Code here runs when the scene is entirely on screen
 
-	end
+    end
 end
 
 
 -- hide()
 function scene:hide( event )
 
-	local sceneGroup = self.view
-	local phase = event.phase
+    local sceneGroup = self.view
+    local phase = event.phase
 
-	if ( phase == "will" ) then
-		-- Code here runs when the scene is on screen (but is about to go off screen)
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is on screen (but is about to go off screen)
 
-	elseif ( phase == "did" ) then
-		-- Code here runs immediately after the scene goes entirely off screen
+    elseif ( phase == "did" ) then
+        -- Code here runs immediately after the scene goes entirely off screen
 
-	end
+    end
 end
 
 
 -- destroy()
 function scene:destroy( event )
 
-	local sceneGroup = self.view
-	-- Code here runs prior to the removal of scene's view
+    local sceneGroup = self.view
+    -- Code here runs prior to the removal of scene's view
 
 end
 
