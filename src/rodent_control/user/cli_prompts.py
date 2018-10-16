7cf47103b00483648fb561002e94b5e2baba5d65
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-10-16 16:42:40 +0100

    pep8 adherence

--- src/rodent_control/user/cli_prompts.py
+++ src/rodent_control/user/cli_prompts.py
@@ -4,9 +4,9 @@ import json
 
 def all_settings(as_json=True):
     all_settings = {
-            'task': task_settings(as_json=False),
-            'session': session_settings(as_json=False)
-        }
+        'task': task_settings(as_json=False),
+        'session': session_settings(as_json=False)
+    }
     if as_json:
         return json.dumps(all_settings)
     return all_settings
@@ -14,14 +14,14 @@ def all_settings(as_json=True):
 
 def task_settings(as_json=True):
     task_settings = {
-            'targets': int(input("Number of targets: ")),
-            'width': int(input("Target width (mm): ")),
-            'distance': int(input("Distance between targets (mm): ")),
-            'delay': int(input("Presentation delay (ms): ")),
-            'haptics': False if input("Haptic feedback (Y/N): ") == 'N' else True,
-            'animated': True if input("Animated (Y/N): ") == 'Y' else False,
-            'shrinking': True if input("Shrinking (Y/N): ") == 'Y' else False
-        }
+        'targets': int(input("Number of targets: ")),
+        'width': int(input("Target width (mm): ")),
+        'distance': int(input("Distance between targets (mm): ")),
+        'delay': int(input("Presentation delay (ms): ")),
+        'haptics': False if input("Haptic feedback (Y/N): ") == 'N' else True,
+        'animated': True if input("Animated (Y/N): ") == 'Y' else False,
+        'shrinking': True if input("Shrinking (Y/N): ") == 'Y' else False
+    }
     if as_json:
         return json.dumps(task_settings)
     return task_settings
@@ -29,12 +29,12 @@ def task_settings(as_json=True):
 
 def session_settings(as_json=True):
     session_settings = {
-            'sessionType': 'rpi',
-            'duration': float(input("Session duration (min): ")),
-            'showResults': False,
-            'playToneAtEnd': True if input("Tone at End (Y/N): ") == 'Y' else False,
-            'saveSession': True
-        }
+        'sessionType': 'rpi',
+        'duration': float(input("Session duration (min): ")),
+        'showResults': False,
+        'playToneAtEnd': True if input("Tone at End (Y/N): ") == 'Y' else False,
+        'saveSession': True
+    }
     if as_json:
         return json.dumps(session_settings)
     return session_settings
