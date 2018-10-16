cde5ea3369449f1065f6e7f448d2dca75771db24
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-10-16 17:33:42 +0100

    HACK only engage visuomotor task for now....

--- rpi-client.lua
+++ rpi-client.lua
@@ -86,10 +86,12 @@ local function rpiLoop(client, ip, port)
             for i, thisData in ipairs(allData) do
                 print("received: ", thisData)
                 if string.match(thisData, "{*}") then  -- does it look like json?
-                    settings = parseSettings(thisData)
-                    composer.setVariable("taskSettings", settings.task)
-                    composer.setVariable("sessionSettings", settings.session)
-                    composer.gotoScene("task")
+                    --settings = parseSettings(thisData)
+                    --composer.setVariable("taskSettings", settings.task)
+                    --composer.setVariable("sessionSettings", settings.session)
+                    --composer.gotoScene("task")
+                    -- HACK only initiate visuomotor task for now
+                    composer.gotoScene("task-visuomotor")
                 end
             end
         end
