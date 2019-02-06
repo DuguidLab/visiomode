3fc448cfb7c049da71e4f12ef77d01483cb83e1b
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2019-02-06 11:21:29 +0000

    pep8 adherence

--- bin/listener.py
+++ bin/listener.py
@@ -1,6 +1,6 @@
 import socketserver
 import carie_controller.core as rc
-import carie_controller.user.cli_prompts as usr
+import carie_controller.interface.cli.cli_prompts as usr
 import datetime
 import json
 
@@ -9,6 +9,7 @@ class TCPHandler(socketserver.BaseRequestHandler):
     """
     The request handler class for the TCP listener server.
     """
+
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.settings = usr.all_settings(as_json=False)
@@ -16,31 +17,40 @@ class TCPHandler(socketserver.BaseRequestHandler):
             self.data = self.request.recv(1024)  # Only suitable for single clients!
             if not self.data:
                 break
-            print("{}:{} wrote: {}".format(self.client_address[0], self.client_address[1], str(self.data, 'utf-8')))
-            if str(self.data, 'utf-8') == 'connect' or str(self.data, 'utf-8') == 'restore':
+            print(
+                "{}:{} wrote: {}".format(
+                    self.client_address[0],
+                    self.client_address[1],
+                    str(self.data, "utf-8"),
+                )
+            )
+            if (
+                str(self.data, "utf-8") == "connect"
+                or str(self.data, "utf-8") == "restore"
+            ):
                 # TODO assign session ID
-                self.request.sendall(b'connected\n')
+                self.request.sendall(b"connected\n")
                 self.start_session()
-            if self.data == b'test':
-                print('test water reward')
+            if self.data == b"test":
+                print("test water reward")
                 rc.water_reward(delay=1000)
-            if str(self.data, 'utf-8').startswith('reward'):
-                print('dispensing reward')
-                rc.water_reward(delay=self.settings['task']['delay'])
-            if str(self.data, 'utf-8').startswith('session'):
+            if str(self.data, "utf-8").startswith("reward"):
+                print("dispensing reward")
+                rc.water_reward(delay=self.settings["task"]["delay"])
+            if str(self.data, "utf-8").startswith("session"):
                 self.end_session()
                 self.start_session()  # Start new one
 
     def start_session(self):
         input("Press ENTER to begin session...")
-        self.request.sendall(bytes(json.dumps(self.settings) + '\n', 'utf-8'))
-        print('---Session Start---')
+        self.request.sendall(bytes(json.dumps(self.settings) + "\n", "utf-8"))
+        print("---Session Start---")
 
     def end_session(self):
-        print('---Session End---')
+        print("---Session End---")
         path = input("Saved sessions directory: ") or "."
-        _, timestamp, session_data = str(self.data, 'utf-8').split(':', 2)
-        with open(path + '/' + timestamp + '.json', 'w') as f:
+        _, timestamp, session_data = str(self.data, "utf-8").split(":", 2)
+        with open(path + "/" + timestamp + ".json", "w") as f:
             try:
                 json.dump(json.loads(session_data), f)
             except Exception as e:
@@ -48,8 +58,8 @@ class TCPHandler(socketserver.BaseRequestHandler):
                 print(session_data, file=f)
 
 
-if __name__ == '__main__':
-    BIND_IP = '0.0.0.0'
+if __name__ == "__main__":
+    BIND_IP = "0.0.0.0"
     BIND_PORT = 5000
 
     socketserver.TCPServer.allow_reuse_address = True
