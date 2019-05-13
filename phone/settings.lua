4b1605e759982cab3fa945a07def56d9d6b63f2e
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2019-05-13 16:18:12 +0100

    save and load settings to / from settings.json file

--- phone/settings.lua
+++ phone/settings.lua
@@ -1,20 +1,23 @@
 local composer = require("composer")
-local json = require("json")
+local saveload = require("saveload")
 
 local scene = composer.newScene()
 
-local docs = system.DocumentsDirectory
+local settingsFile = "settings.json"
+
+local controllerAddress, controllerPort  -- vars to hold user input
 
 local function gotoMenu()
     composer.gotoScene("menu")
 end
 
-local function loadSettings()
-
-end
-
 local function saveSettings()
-
+    local settings = {
+        address = controllerAddress.text,
+        port = controllerPort.text
+    }
+    saveload.saveTable(settings, settingsFile)
+    gotoMenu()
 end
 
 
@@ -25,6 +28,16 @@ end
 -- create()
 function scene:create(event)
 
+    -- Get existing settings or use defaults
+    local settings = saveload.loadTable(settingsFile)
+    if not settings then
+        settings = {
+            address = "0.0.0.0",
+            port = "5000",
+        }
+    end
+
+    -- Interface setup
     local sceneGroup = self.view
 
     local background = display.newImageRect(sceneGroup, "assets/menu-background.jpg", 1400, 800)
@@ -36,16 +49,16 @@ function scene:create(event)
     local ipTitle = display.newText(sceneGroup, "Controller IP: ", display.contentCenterX - 200, 300, native.SystemFont, 40)
     ipTitle.anchorX = 1
 
-    rpiServer = native.newTextField(display.contentCenterX + 200, 300, 300, 35)
-    rpiServer.text = "0.0.0.0"
-    sceneGroup:insert(rpiServer)
+    controllerAddress = native.newTextField(display.contentCenterX + 200, 300, 300, 35)
+    controllerAddress.text = settings.address
+    sceneGroup:insert(controllerAddress)
 
     local portTitle = display.newText(sceneGroup, "Controller Port: ", display.contentCenterX - 200, 375, native.SystemFont, 40)
     portTitle.anchorX = 1
 
-    rpiSocket = native.newTextField(display.contentCenterX + 200, 375, 100, 35)
-    rpiSocket.text = "5000"
-    sceneGroup:insert(rpiSocket)
+    controllerPort = native.newTextField(display.contentCenterX + 200, 375, 100, 35)
+    controllerPort.text = settings.port
+    sceneGroup:insert(controllerPort)
 
     local setupSessionButton = display.newText(sceneGroup, "Save Settings", display.contentCenterX - 200, 700, native.SystemFont, 45)
     setupSessionButton:addEventListener("tap", saveSettings)
