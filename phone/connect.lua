3ca45dd09ab59382283daf1267b3f50fa1ff51ff
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2019-05-13 16:35:13 +0100

    connectivity fields prepopulated with settings.json params if they exist, resolves #3

--- phone/connect.lua
+++ phone/connect.lua
@@ -1,17 +1,16 @@
 
 local composer = require( "composer" )
+local saveload = require("saveload")
 
 local scene = composer.newScene()
 
--- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
--- -----------------------------------------------------------------------------------
+local settingsFile = "settings.json"
+
 
 local rpiClient = require("rpi-client")
 local widget = require( "widget" )
-local rpiServer
-local rpiSocket
+local controllerAddress
+local controllerPort
 
 local connectButton
 local connectionMessage
@@ -19,14 +18,14 @@ local connectionMessage
 
 local function connectionWrapper()
     connectButton.alpha = 0
-    client = rpiClient.rpiConnect(rpiServer.text, rpiSocket.text)
+    client = rpiClient.rpiConnect(controllerAddress.text, controllerPort.text)
     if client then
         print("Can connect!")
         connectionMessage.text = "Successfully connected to RPi Server"
-        rpiClient.rpiLoop(client, rpiServer.text, rpiSocket.text)
+        rpiClient.rpiLoop(client, controllerAddress.text, controllerPort.text)
     else
-        print("Cannot connect to " .. rpiServer.text)
-        connectionMessage.text = "Could not connect to " .. rpiServer.text .. " on port " .. rpiSocket.text
+        print("Cannot connect to " .. controllerAddress.text)
+        connectionMessage.text = "Could not connect to " .. controllerAddress.text .. " on port " .. controllerPort.text
         connectButton.alpha = 1
     end
 end
@@ -38,6 +37,14 @@ end
 
 -- create()
 function scene:create( event )
+    -- Get existing settings or use defaults
+    local settings = saveload.loadTable(settingsFile)
+    if not settings then
+        settings = {
+            address = "0.0.0.0",
+            port = "5000",
+        }
+    end
 
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
@@ -48,21 +55,21 @@ function scene:create( event )
 
     local title = display.newText(sceneGroup, "Connect to Raspberry Pi Controller", display.contentCenterX, 70, native.SystemFont, 50)
 
-    local subtitle = display.newText(sceneGroup, "Make sure that phone and RPi are connected to the same network", display.contentCenterX, 150, native.SystemFont, 42)
+    local subtitle = display.newText(sceneGroup, "Make sure that phone and controller are connected to the same network", display.contentCenterX, 150, native.SystemFont, 42)
 
-    local ipTitle = display.newText(sceneGroup, "RPi IP: ", display.contentCenterX-200, 300, native.SystemFont, 40)
+    local ipTitle = display.newText(sceneGroup, "Controller Address: ", display.contentCenterX-200, 300, native.SystemFont, 40)
     ipTitle.anchorX = 1
 
-    rpiServer = native.newTextField(display.contentCenterX+200, 300, 300, 35)
-    rpiServer.text = "192.168.0.1"
-    sceneGroup:insert(rpiServer)
+    controllerAddress = native.newTextField(display.contentCenterX+200, 300, 300, 35)
+    controllerAddress.text = settings.address
+    sceneGroup:insert(controllerAddress)
 
-    local portTitle = display.newText(sceneGroup, "RPi Port: ", display.contentCenterX-200, 375, native.SystemFont, 40)
+    local portTitle = display.newText(sceneGroup, "Controller Port: ", display.contentCenterX-200, 375, native.SystemFont, 40)
     portTitle.anchorX = 1
 
-    rpiSocket = native.newTextField(display.contentCenterX+200, 375, 100, 35)
-    rpiSocket.text = "5000"
-    sceneGroup:insert(rpiSocket)
+    controllerPort = native.newTextField(display.contentCenterX+200, 375, 100, 35)
+    controllerPort.text = settings.port
+    sceneGroup:insert(controllerPort)
 
     connectionMessage = display.newText(sceneGroup, "", display.contentCenterX, 550, native.SystemFont, 42)
 
