03f8cb96bdcef6ab9e3dff6e22adea1cefa57e11
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2019-05-13 15:19:30 +0100

    interface ip and port boxes

--- phone/settings.lua
+++ phone/settings.lua
@@ -1,44 +1,20 @@
 local composer = require("composer")
+local json = require("json")
 
 local scene = composer.newScene()
 
--- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
--- -----------------------------------------------------------------------------------
-local widget = require("widget")
-
-local taskSettings
-
-local animatedTarget
-local shrinkingTarget
-local targetWidth
-local delay
-local numTargets
-local targetDistance
-local hapticsOnMiss
-
-local animatedTargetOptions
-local shrinkingTargetOptions
-
-local goBackButton
+local docs = system.DocumentsDirectory
 
 local function gotoMenu()
     composer.gotoScene("menu")
 end
 
-local function setupSession()
-    -- export task settings
-    taskSettings = {
-        delay = delay.text,
-        width = targetWidth.text,
-        targets = numTargets.text,
-        distance = targetDistance.text,
-        haptics = hapticsOnMiss.isOn,
-    }
-    delay = native.newTextField(display.contentCenterX + 250, 150, 100, 35)
-    --table.foreach(taskSettings, print) -- debug
-    composer.setVariable("taskSettings", taskSettings)
+local function loadSettings()
+
+end
+
+local function saveSettings()
+
 end
 
 
@@ -55,78 +31,27 @@ function scene:create(event)
     background.x = display.contentCenterX
     background.y = display.contentCenterY
 
-    local title = display.newText(sceneGroup, "Stand-Alone Task Setup", display.contentCenterX, 70, native.SystemFont, 50)
+    local title = display.newText(sceneGroup, "Settings", display.contentCenterX, 70, native.SystemFont, 50)
 
-    local delayTitle = display.newText(sceneGroup, "Presentation Delay (ms): ", display.contentCenterX - 20, 150, native.SystemFont, 40)
-    delayTitle.anchorX = 1
+    local ipTitle = display.newText(sceneGroup, "Controller IP: ", display.contentCenterX - 200, 300, native.SystemFont, 40)
+    ipTitle.anchorX = 1
 
-    delay = native.newTextField(display.contentCenterX + 250, 150, 100, 35)
-    delay.text = 500
-    delay.anchorX = 0
-    sceneGroup:insert(delay)
+    rpiServer = native.newTextField(display.contentCenterX + 200, 300, 300, 35)
+    rpiServer.text = "0.0.0.0"
+    sceneGroup:insert(rpiServer)
 
-    local numTargetsTitle = display.newText(sceneGroup, "Number of Targets: ", display.contentCenterX - 20, 225, native.SystemFont, 40)
-    numTargetsTitle.anchorX = 1
+    local portTitle = display.newText(sceneGroup, "Controller Port: ", display.contentCenterX - 200, 375, native.SystemFont, 40)
+    portTitle.anchorX = 1
 
-    numTargets = native.newTextField(display.contentCenterX + 250, 225, 100, 35)
-    numTargets.text = 2
-    numTargets.anchorX = 0
-    sceneGroup:insert(numTargets)
+    rpiSocket = native.newTextField(display.contentCenterX + 200, 375, 100, 35)
+    rpiSocket.text = "5000"
+    sceneGroup:insert(rpiSocket)
 
-    targetDistanceTitle = display.newText(sceneGroup, "Min Target Distance (mm): ", display.contentCenterX - 20, 300, native.SystemFont, 40)
-    targetDistanceTitle.anchorX = 1
+    local setupSessionButton = display.newText(sceneGroup, "Save Settings", display.contentCenterX - 200, 700, native.SystemFont, 45)
+    setupSessionButton:addEventListener("tap", saveSettings)
 
-    targetDistance = native.newTextField(display.contentCenterX + 250, 300, 100, 35)
-    targetDistance.text = 20
-    targetDistance.anchorX = 0
-    sceneGroup:insert(targetDistance)
-
-    local targetWidthTitle = display.newText(sceneGroup, "Target Width (mm): ", display.contentCenterX - 20, 375, native.SystemFont, 40)
-    targetWidthTitle.anchorX = 1
-
-    targetWidth = native.newTextField(display.contentCenterX + 250, 375, 100, 35)
-    targetWidth.text = 5
-    targetWidth.anchorX = 0
-    sceneGroup:insert(targetWidth)
-
-    local animatedTargetTitle = display.newText(sceneGroup, "Animated Target: ", display.contentCenterX - 20, 450, native.SystemFont, 40)
-    animatedTargetTitle.anchorX = 1
-
-    animatedTarget = widget.newSwitch({
-        x = display.contentCenterX + 295,
-        y = 450,
-        style = "checkbox",
-        id = "animatedTargetCheckbox",
-        onPress = onAnimatedTargetSwitchPress
-    })
-    sceneGroup:insert(animatedTarget)
-
-    local shrinkingTargetTitle = display.newText(sceneGroup, "Shrinking Target: ", display.contentCenterX - 20, 525, native.SystemFont, 40)
-    shrinkingTargetTitle.anchorX = 1
-
-    shrinkingTarget = widget.newSwitch({
-        x = display.contentCenterX + 295,
-        y = 525,
-        style = "checkbox",
-        id = "shrinkingTargetCheckbox",
-        onPress = onShrinkingTargetSwitchPress
-    })
-    sceneGroup:insert(shrinkingTarget)
-
-    local hapticsOnMissTitle = display.newText(sceneGroup, "Vibrate on Miss: ", display.contentCenterX - 20, 600, native.SystemFont, 40)
-    hapticsOnMissTitle.anchorX = 1
-
-    hapticsOnMiss = widget.newSwitch({
-        x = display.contentCenterX + 295,
-        y = 600,
-        style = "checkbox",
-        id = "shrinkingTargetCheckbox",
-        initialSwitchState = true,
-    })
-    sceneGroup:insert(hapticsOnMiss)
-
-    local setupSessionButton = display.newText(sceneGroup, "Setup Session", display.contentCenterX, 700, native.SystemFont, 45)
-    setupSessionButton:addEventListener("tap", setupSession)
+    local goBackButton = display.newText(sceneGroup, "Go Back", display.contentCenterX + 200, 700, native.SystemFont, 45)
+    goBackButton:addEventListener("tap", gotoMenu)
 end
 
 
