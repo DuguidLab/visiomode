9df3d5e7c6b4fe0e2844e68208339f519fb65395
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-01-03 18:34:35 +0000

    split webapp and gui into separate modules to make life easier

--- src/visiomode/gui/main.py
+++ src/visiomode/gui/main.py
@@ -1,20 +1,13 @@
-#!/usr/bin/env python3
-
-"""Main application entry point"""
+"""GUI Application entry point"""
 
 #  This file is part of visiomode.
 #  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
 
-import redis as rds
 import pyglet as pgt
-import visiomode.config as cfg
 
 
 def main():
-    config = cfg.Config()
-    redis = rds.Redis(host=config.redis_host, port=config.redis_port)
-
     # Dummy hello world pyglet window
     window = pgt.window.Window()
     label = pgt.text.Label('Hello, world',
