d2b5f5b799b30692b7b171a0a7523b00fba38961
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-03-11 16:25:04 +0000

    add handler for redis status updates

--- src/visiomode/gui/main.py
+++ src/visiomode/gui/main.py
@@ -4,25 +4,54 @@
 #  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
 
-import pyglet as pgt
+import redis
+import pyglet
+import visiomode.config as cfg
+
+
+config = cfg.Config()
+rds = redis.Redis(host=config.redis_host, port=config.redis_port)
+
+
+class RedisEventHandler(pyglet.event.EventDispatcher):
+    def __init__(self):
+        super(RedisEventHandler, self).__init__()
+        self.session_sub = rds.pubsub()
+        self.session_sub.psubscribe(**{"__key*__:status": self.get_status})
+        self.session_thread = self.session_sub.run_in_thread(sleep_time=0.01)
+
+    def get_status(self, *args, **kwargs):
+        status = rds.get('status').decode("utf8")
+        print("Status is {}".format(status))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Cleans up resources before instance is destroyed.
+
+        Stop the Redis listener thread to allow the class to be garbage collected.
+        """
+        self.session_thread.stop()
 
 
 def main():
     # Dummy hello world pyglet window
-    window = pgt.window.Window()
-    label = pgt.text.Label('Hello, world',
-                           font_name='Times New Roman',
-                           font_size=36,
-                           x=window.width // 2, y=window.height // 2,
-                           anchor_x='center', anchor_y='center')
+    window = pyglet.window.Window()
+    label = pyglet.text.Label('Hello, world',
+                              font_name='Times New Roman',
+                              font_size=36,
+                              x=window.width // 2, y=window.height // 2,
+                              anchor_x='center', anchor_y='center')
 
     @window.event
     def on_draw():
         window.clear()
         label.draw()
 
-    pgt.app.run()
+    pyglet.app.run()
 
 
 if __name__ == '__main__':
-    main()
+    with RedisEventHandler() as r:
+        main()
