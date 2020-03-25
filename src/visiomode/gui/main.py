c25c43cd3cddbd75e842920b27628717d599fc7a
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-03-25 18:42:12 +0000

    implement a redis event dispatcher that doesn't crash with SIGSERV (for now..)

--- src/visiomode/gui/main.py
+++ src/visiomode/gui/main.py
@@ -4,52 +4,41 @@
 #  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
 
-import redis
 import pyglet
+import redis
 import visiomode.config as cfg
-
+from visiomode.gui.events import RedisEventDispatch
 
 config = cfg.Config()
 rds = redis.Redis(host=config.redis_host, port=config.redis_port)
 
 
-class RedisEventHandler(pyglet.event.EventDispatcher):
-    def __init__(self):
-        super(RedisEventHandler, self).__init__()
-        self.session_sub = rds.pubsub()
-        self.session_sub.psubscribe(**{"__key*__:status": self.get_status})
-        self.session_thread = self.session_sub.run_in_thread(sleep_time=0.01, daemon=True)
-
-    def get_status(self, *args, **kwargs):
-        status = rds.get('status').decode("utf8")
-        print("Status is {}".format(status))
-        self.dispatch_event('on_status_update')
-
-
-RedisEventHandler.register_event_type('on_status_update')
-
-
 def main():
-    # Dummy hello world pyglet window
     window = pyglet.window.Window(resizable=True)
     main_batch = pyglet.graphics.Batch()
-
-    redis_handler = RedisEventHandler()
 
     label = pyglet.text.Label('Hello, world',
                               font_name='Times New Roman',
                               font_size=36,
                               x=window.width // 2, y=window.height // 2,
-                              anchor_x='center', anchor_y='center', batch=main_batch)
+                              anchor_x='center', anchor_y='center')
 
-    @redis_handler.event
-    def on_status_update():
-        print(label.text)
+    @window.event
+    def on_mouse_press(x, y, button, modifiers):
+        if button == pyglet.window.mouse.LEFT:
+            print('The left mouse button was pressed.')
 
     @window.event
     def on_draw():
         window.clear()
         main_batch.draw()
+
+    redis_dispatch = RedisEventDispatch()
+
+    @redis_dispatch.event
+    def on_update():
+        print("please work")
+        pyglet.app.exit()
 
     pyglet.app.run()
 
