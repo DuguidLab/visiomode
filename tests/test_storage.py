27f4b617fa1edb486d7a4d4dbb12af8909ab02ee
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-04-09 18:31:38 +0100

    add status subscription tests

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -8,9 +8,28 @@ class TestRedisClient:
     rds = storage.RedisClient()
 
     def test_status_updating(self):
+        """Test getting and setting status"""
         status = "test"
         self.rds.set_status(status)
         assert self.rds.get_status() == status
 
-    def test_subscribe_status(self):
-        assert True
+    def test_subscribe_status_nocallback(self):
+        """Test status subscription without a callback"""
+        # subscribe
+        status_sub = self.rds.subscribe_status(threaded=False)
+        # change the status
+        self.rds.set_status("test")
+        # see if the change was picked up, if not get_message will return None
+        assert status_sub.get_message()
+
+    def test_subscribe_status_callback(self):
+        """Test a threaded status subscription with a callback"""
+
+        def callback(_):
+            """Dummy callback, if this is called the test passes"""
+            assert True
+
+        # subscribe
+        status_sub = self.rds.subscribe_status(callback=callback, threaded=True)
+        # change the status - if update is picked up the callback should pop up
+        self.rds.set_status("test")
