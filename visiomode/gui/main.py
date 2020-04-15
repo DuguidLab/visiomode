fe9f79ed7f461be9f4874f3284dc35d9473c82d2
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-04-15 22:24:41 +0100

    switch to pygame, pyglet crashes left and right and it has annoyed me to no end

--- visiomode/gui/main.py
+++ visiomode/gui/main.py
@@ -4,57 +4,92 @@
 #  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
 
-import pyglet
+import pygame
+import pygame.locals
 import visiomode.storage as storage
 import visiomode.models as models
+import visiomode.gui.stimuli as stim
 
 import faulthandler  # report segmentation faults as tracebacks
 
 faulthandler.enable()
 
-rds = storage.RedisClient()
-
 
 def main():
-    window = pyglet.window.Window(resizable=True)
-    main_batch = pyglet.graphics.Batch()
+    # Initialise screen
+    pygame.init()
+    screen = pygame.display.set_mode((600, 400))
+    pygame.display.set_caption("Basic Pygame program")
 
-    status_sub = rds.subscribe_status(threaded=False)
-    session = None
+    # Fill background
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((250, 250, 250))
 
-    label = pyglet.text.Label(
-        "Hello, world",
-        font_name="Times New Roman",
-        font_size=36,
-        x=window.width // 2,
-        y=window.height // 2,
-        anchor_x="center",
-        anchor_y="center",
-        batch=main_batch,
-    )
+    # Display some text
+    font = pygame.font.Font(None, 36)
+    text = font.render("Hello There", 1, (10, 10, 10))
+    textpos = text.get_rect()
+    textpos.centerx = background.get_rect().centerx
+    background.blit(text, textpos)
 
-    @window.event
-    def on_mouse_press(x, y, button, modifiers):
-        if button == pyglet.window.mouse.LEFT:
-            print("The left mouse button was pressed.")
+    # Blit everything to the screen
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
 
-    @window.event
-    def on_draw():
-        window.clear()
-        main_batch.draw()
+    # Event loop
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.locals.QUIT:
+                return
 
-    def update_status(dt):
-        if not status_sub.get_message():
-            return
-        status = rds.get_status()
-        if status == "test":
-            label.text = "something"
-        if status == "requested":
-            label.text = "started!"
-            # session = models.Session()
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
 
-    pyglet.clock.schedule_interval(update_status, 1 / 60)
-    pyglet.app.run()
+    # window = pyglet.window.Window(resizable=True)
+    # main_batch = pyglet.graphics.Batch()
+    #
+    # status_sub = rds.subscribe_status(threaded=False)
+    # session = None
+    #
+    # label = pyglet.text.Label(
+    #     "Hello, world",
+    #     font_name="Times New Roman",
+    #     font_size=36,
+    #     x=window.width // 2,
+    #     y=window.height // 2,
+    #     anchor_x="center",
+    #     anchor_y="center",
+    #     batch=main_batch,
+    # )
+    #
+    # @window.event
+    # def on_mouse_press(x, y, button, modifiers):
+    #     if button == pyglet.window.mouse.LEFT:
+    #         print("The left mouse button was pressed.")
+    #
+    # @window.event
+    # def on_draw():
+    #     window.clear()
+    #     main_batch.draw()
+    #
+    # def update_status(dt):
+    #     if not status_sub.get_message():
+    #         return
+    #     status = rds.get_status()
+    #     print("updating...")
+    #     if status == storage.DEBUG:
+    #         label.text = "DEBUG"
+    #     if status == storage.REQUESTED:
+    #         label.text = "started!"
+    #         # get request
+    #         request = rds.get_session_request()
+    #         print(request)
+    #         task = pyglet.window.Window(resizable=True)
+    #         rds.set_status(storage.ACTIVE)
+    #
+    # pyglet.clock.schedule_interval(update_status, 1 / 60)
+    # pyglet.app.run()
 
 
 if __name__ == "__main__":
