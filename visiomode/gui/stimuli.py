0f6cba68b477556dd59b6dc5fbf1986a1fd818e0
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-04-21 16:32:02 +0100

    generate stimulus image from numpy array

--- visiomode/gui/stimuli.py
+++ visiomode/gui/stimuli.py
@@ -3,26 +3,31 @@
 #  This file is part of visiomode.
 #  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
+
+#  This file is part of visiomode.
+#  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
+#  Distributed under the terms of the MIT Licence.
 import os
-import pygame
+import numpy as np
+import pygame as pg
 
 
 def load_image(name):
     """ Load image and return image object"""
     fullname = os.path.join("visiomode/gui/res", name)
     try:
-        image = pygame.image.load(fullname)
+        image = pg.image.load(fullname)
         if image.get_alpha is None:
             image = image.convert()
         else:
             image = image.convert_alpha()
-    except pygame.error as message:
+    except pg.error as message:
         print("Cannot load image:", fullname)
         raise SystemExit(message)
     return image, image.get_rect()
 
 
-class BaseStimulus(pygame.sprite.Sprite):
+class BaseStimulus(pg.sprite.Sprite):
     def __init__(self, *args):
         super().__init__(*args)
 
@@ -30,6 +35,11 @@ class BaseStimulus(pygame.sprite.Sprite):
 class Grating(BaseStimulus):
     def __init__(self, x, y, *args):
         super().__init__(*args)
-        self.image, self.rect = load_image("target.jpg")
-        screen = pygame.display.get_surface()
+        array = np.zeros((600, 400, 3), np.int32)
+        array[:] = (0, 0, 0)
+        array[:, ::5] = (255, 255, 255)
+        self.image = pg.surfarray.make_surface(array)
+        self.rect = self.image.get_rect()
+        # self.image, self.rect = load_image("target.jpg")
+        screen = pg.display.get_surface()
         self.area = screen.get_rect()
