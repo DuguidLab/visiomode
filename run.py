181ff9c7ad82609b540b9824e0bb3a877c82c54b
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-04-29 17:08:50 +0100

    set up macos packaging with py2app

---
+++ run.py
@@ -0,0 +1,8 @@
+"""Application entry point."""
+
+#  This file is part of visiomode.
+#  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
+#  Distributed under the terms of the MIT Licence.
+import visiomode.main
+
+visiomode.main.main()
