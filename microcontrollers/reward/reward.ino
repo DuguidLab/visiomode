12eeab78a029bfa48ddb02295bc1d55155256d75
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-07-28 15:42:40 +0100

    move servo on serial input

--- microcontrollers/reward/reward.ino
+++ microcontrollers/reward/reward.ino
@@ -1,7 +1,15 @@
+#include <Servo.h>
+#define SERVO_PIN 9
+
+Servo spoutServo;
+
+int servoAngle = 5;
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  spoutServo.attach(SERVO_PIN, 1000, 2000);
 }
 
 void loop() {
@@ -11,11 +19,15 @@ void loop() {
     if (state == 'H' || state == 'h') {
       digitalWrite(LED_BUILTIN, HIGH);
       Serial.println("LED ON");
+      spoutServo.write(170);
+      delay(500);
     }
     if (state == 'L' || state == 'l') {
       digitalWrite(LED_BUILTIN, LOW);
       Serial.println("LED OFF");
+      spoutServo.write(10);
+      delay(500);
     }
   }
-  delay(50);
+  delay(50); // allow buffer to fill
 }
