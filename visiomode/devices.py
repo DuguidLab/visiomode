57e697cdfb1ba5f57ed2595ad4807f8a9103793b
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-07-28 18:56:23 +0100

    add BaseDevice class with some handy functions

---
+++ visiomode/devices.py
@@ -0,0 +1,54 @@
+"""Communications module for external microcontrollers over the serial bus"""
+
+#  This file is part of visiomode.
+#  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
+#  Distributed under the terms of the MIT Licence.
+import re
+import json
+import serial
+
+
+class BaseDevice:
+    address = None
+
+    def __init__(self, address):
+        self.address = address
+
+    @classmethod
+    def get_common_name(cls):
+        """"Return the human-readable, space-separated name for the class."""
+        return re.sub(r"((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))", r" \1", cls.__name__)
+
+    @classmethod
+    def get_children(cls):
+        """Return all inheriting children as a generator."""
+        for child in cls.__subclasses__():
+            yield from child.get_children()
+            yield child
+
+    @classmethod
+    def get_identifier(cls):
+        return cls.__name__.lower()
+
+    def __repr__(self):
+        return "<{} device at {}>".format(self.get_common_name(), self.address)
+
+
+class InputDevice(BaseDevice):
+    pass
+
+
+class OutputDevice(BaseDevice):
+    pass
+
+
+class WaterReward(OutputDevice):
+    def __init__(self, address):
+        super().__init__(address)
+
+    def dispense(self):
+        pass
+
+
+class DeviceError(Exception):
+    pass
