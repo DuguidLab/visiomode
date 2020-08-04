219aed2580040a389801b9369a76bedcf6bd0376
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-08-04 11:24:22 +0100

    add config loader for device.yml files

--- visiomode/devices.py
+++ visiomode/devices.py
@@ -3,17 +3,48 @@
 #  This file is part of visiomode.
 #  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
+import os
 import re
 import json
+import yaml
 import serial
+import logging
 import threading
+import visiomode.config as conf
 
 
 class BaseDevice:
     address = None
 
-    def __init__(self, address):
+    def __init__(self, address, config_path=None):
         self.address = address
+        self.config_path = config_path or (
+            conf.Config().devices + os.sep + self.get_identifier() + ".yml"
+        )
+
+        # if a config file exists, load it
+        if os.path.exists(self.config_path):
+            self.load()
+
+    def load(self):
+        """Loads YAML file parameters as class attributes."""
+        with open(self.config_path) as f:
+            config = yaml.safe_load(f)
+            for key, value in config.items():
+                if key not in self.__dict__.keys():
+                    logging.info(
+                        "{} is not a valid config parameter, skipping...".format(key)
+                    )
+                    continue
+                setattr(self, key, value)
+
+    def save(self):
+        """Save device configuration."""
+        pass
+
+    def calibrate(self):
+        """Calibrate device parameters."""
+        pass
 
     @classmethod
     def get_common_name(cls):
@@ -44,6 +75,11 @@ class OutputDevice(BaseDevice):
 
 
 class WaterReward(OutputDevice):
+    servo_start = 10
+    servo_end = 170
+    movt_delay = 500
+    pump_ms = 50
+
     def __init__(self, address, threaded=True):
         super().__init__(address)
         self.threaded = threaded
