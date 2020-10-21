d34e00096126c0d7931e5661b9d3dcfd9d3252b1
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2020-10-21 19:24:09 +0100

    add test for Grating

---
+++ tests/test_stimuli.py
@@ -0,0 +1,36 @@
+"""Test stimulus generation"""
+
+import pytest
+import pygame as pg
+import visiomode.stimuli as stimuli
+
+
+class TestStimulusGeneration:
+    """Test stimulus classes."""
+
+    pg.init()
+    screen = pg.display.set_mode((720, 720))
+    background = pg.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+
+    def test_grating(self):
+        grating = stimuli.Grating(self.background)
+        grating.show()
+        pg.display.flip()
+
+    def test_moving_grating(self):
+        pass
+
+    def test_solid_colour(self):
+        pass
+
+    def test_isoluminant_gray(self):
+        pass
+
+
+class TestStimulusHelpers:
+    """Test module helper functions."""
+
+    pass
