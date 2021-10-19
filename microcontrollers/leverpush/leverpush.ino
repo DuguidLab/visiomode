8ccfbd17809f68cc2b02d825b985e988f8782155
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2021-10-19 17:18:28 +0100

    adjust lever push servo direction

--- microcontrollers/leverpush/leverpush.ino
+++ microcontrollers/leverpush/leverpush.ino
@@ -1,24 +1,25 @@
 #include <Servo.h>
 #define SERVO_PIN 9
-#define BACK_SENSOR_PIN 2
-#define FRONT_SENSOR_PIN 3
-#define POS_LOCKED 20
-#define POS_UNLOCKED 90
+#define BACK_SENSOR_PIN 3
+#define FRONT_SENSOR_PIN 2
+#define POS_LOCKED 90
+#define POS_UNLOCKED 65
 
 Servo leverServo;
 
 int BackSensorState = 0;   // State of IR sensor at original lever position
 int FrontSensorState = 0;  // State of IR sensor at the forward lever position
 
+bool response = false;
+
 void setup() {
     pinMode(BACK_SENSOR_PIN, INPUT_PULLUP);
     pinMode(FRONT_SENSOR_PIN, INPUT_PULLUP);
 
-    Serial.begin(9600);
-
     leverServo.attach(SERVO_PIN, 1000, 2000);
     leverServo.write(POS_LOCKED);
 
+    Serial.begin(9600);
 }
 
 void loop() {
@@ -38,8 +39,15 @@ void loop() {
     FrontSensorState = digitalRead(FRONT_SENSOR_PIN);
 
     // When the lever is moved forward, the front IR sensor is broken and the back one is re-established.
-    if (BackSensorState == HIGH && FrontSensorState == LOW)
+    if (BackSensorState == HIGH && FrontSensorState == LOW && response == false) {
+        response = true;
         Serial.println("R");
+    }
+
+    if (BackSensorState == LOW && FrontSensorState == HIGH) {
+      response = false;
+    }
+
 
     delay(50); // allow buffer to fill
 }
