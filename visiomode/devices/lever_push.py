72f68a1260c86938259b72fd4686160050e410da
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2021-10-20 14:56:40 +0100

    remove write sleep

--- visiomode/devices/lever_push.py
+++ visiomode/devices/lever_push.py
@@ -71,7 +71,6 @@ class LeverPush(devices.InputDevice):
             message = lever_response_map.get(raw_message[0]) if raw_message else None
             if message == "response":
                 self._response_q.put(message)
-                time.sleep(0.1)
             elif message == "error":
                 raise devices.DeviceError("Lever-push controller error.")
 
