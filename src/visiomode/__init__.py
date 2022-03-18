8e9f8d44ec333699d27918b9f8aa83db0feb1714
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2022-03-18 14:38:12 +0000

    refactor entry point to package __init__

--- src/visiomode/__init__.py
+++ src/visiomode/__init__.py
@@ -1,3 +1,13 @@
+"""Application entry point."""
+
 #  This file is part of visiomode.
 #  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
+import faulthandler  # report segmentation faults as tracebacks
+import visiomode.core
+
+faulthandler.enable()
+
+
+def run():
+    visiomode.core.Visiomode()
