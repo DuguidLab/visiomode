f576c4769e5c2e2d2059bc28d1d156d740b145c2
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2022-03-19 18:15:47 +0000

    fix tox with dummy test

---
+++ tests/test_launch.py
@@ -0,0 +1,19 @@
+#  This file is part of visiomode.
+#  Copyright (c) 2022 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
+#  Distributed under the terms of the MIT Licence.
+
+#  This file is part of visiomode.
+#  Copyright (c) 2022 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
+#  Distributed under the terms of the MIT Licence.
+
+import unittest
+import visiomode
+
+
+class AppLaunch(unittest.TestCase):
+    def test_launch(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
