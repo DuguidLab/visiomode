38971185ca55af8dd02888653f9e1609def63e4e
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2022-03-21 18:51:50 +0000

    use setuptools_scm to version app

--- src/visiomode/__init__.py
+++ src/visiomode/__init__.py
@@ -3,8 +3,18 @@
 #  This file is part of visiomode.
 #  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
+
 import faulthandler  # report segmentation faults as tracebacks
 import visiomode.core
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("visiomode")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
 
 faulthandler.enable()
 
