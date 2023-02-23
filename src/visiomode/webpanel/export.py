51228e996303a14a96214a8340edaba6b62dadb5
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2023-02-23 20:09:19 +0000

    use export instead of parser

--- src/visiomode/webpanel/export.py
+++ src/visiomode/webpanel/export.py
@@ -1,12 +1,12 @@
-"""File parsers for session data."""
+"""File parsers for exporting session data."""
 #  This file is part of visiomode.
 #  Copyright (c) 2023 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
 
 
-class CSVParser:
+class NWBExport:
     pass
 
 
-class NWBParser:
+class CSVExport:
     pass
