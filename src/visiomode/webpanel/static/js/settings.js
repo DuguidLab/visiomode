79b323de0f6b1e327e8dfcbe1b1cd4aa9994f26e
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2023-04-28 17:35:47 +0100

    prefill settings field with current options

--- src/visiomode/webpanel/static/js/settings.js
+++ src/visiomode/webpanel/static/js/settings.js
@@ -3,3 +3,19 @@
  * Copyright (c) 2023 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
  * Distributed under the terms of the MIT Licence.
  */
+
+let currentSettings;
+
+function loadSettings() {
+    $.get("/api/settings", function (data) {
+        console.log(data);
+        currentSettings = data;
+        document.getElementById("display-width").value = currentSettings.width;
+        document.getElementById("display-height").value = currentSettings.height;
+        document.getElementById("display-fps").value = currentSettings.fps;
+        document.getElementById("display-fullscreen").value = currentSettings.fullscreen.toString();
+        document.getElementById("storage-path").value = currentSettings.data_dir;
+    });
+}
+
+loadSettings();
