9f4da25689b9a266282d69da398b7d1bbd0ea73d
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2024-04-01 17:44:26 +0100

    change animal id field to drop-down box with animals from database

--- src/visiomode/webpanel/static/js/session.js
+++ src/visiomode/webpanel/static/js/session.js
@@ -243,7 +243,7 @@ function isCanvasBlank(canvas) {
 // });
 
 
-/// Dynamic form updates for protocol selection
+/// Dynamic form updates
 
 // load protocol options on select
 protocol_selector = document.getElementById('protocol');
@@ -255,3 +255,21 @@ protocol_selector.onchange = function () {
 }
 
 protocol_selector.onchange();
+
+
+// load animals
+function loadAnimals() {
+    let animal_selector = document.getElementById('animal_id');
+    $.get("/api/animals").done(function (data) {
+        animal_selector.innerHTML = "";
+        data.animals.reverse(); // reverse order to show latest animals first
+        data.animals.forEach(function (animal) {
+            let option = document.createElement('option');
+            option.value = animal.animal_id;
+            option.text = animal.animal_id;
+            animal_selector.add(option);
+        });
+    });
+}
+
+loadAnimals();
