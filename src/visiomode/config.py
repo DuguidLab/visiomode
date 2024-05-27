add2430beb48f988b11be7a6d889ed583ac9ff80
Olivier Delree <olivierdelree@protonmail.com>
2024-05-27 12:43:36 +0100

    switch config implementation to singleton design pattern
    This revamps the implementation of the `Config` class to a singleton
    approach. Instead of offering a regular constructor to the rest of the
    application, this keeps track of a single static instance of the config
    and returns it on subsequent calls.

--- src/visiomode/config.py
+++ src/visiomode/config.py
@@ -2,17 +2,18 @@
 
 #  This file is part of visiomode.
 #  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
+#  Copyright (c) 2024 Olivier Delree <odelree@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
 
 import os
 import shutil
 import json
 import logging
+import typing
 
 logging.basicConfig(level=logging.INFO)
 
-
-CONFIG_PATH = ".visiomode.json"
+DEFAULT_CONFIG_PATH = ".visiomode.json"
 
 DEFAULT_CONFIG = {
     "debug": True,
@@ -24,11 +25,11 @@ DEFAULT_CONFIG = {
     "width": 400,
     "height": 800,
     "fullscreen": False,
-    "devices": "devices/",
+    "devices": "devices",
 }
 
 
-class Config:
+class Config(object):
     """App configuration class.
 
     This class loads and saves configuration from a JSON file. If no config file exists, it creates one with default values.
@@ -38,92 +39,120 @@ class Config:
         flask_key: Flask secret key.
         data_dir: Path to data directory.
         cache_dir: Path to cache directory.
+        db_dir: Path to database directory.
         fps: Screen refresh rate in frames per second.
         width: Screen width.
         height: Screen height.
         fullscreen: Fullscreen mode flag.
         devices: Path to devices directory.
-        db_dir: Path to database directory.
     """
 
     debug: bool
     flask_key: str
     data_dir: str
     cache_dir: str
+    db_dir: str
     fps: int
     width: int
     height: int
     fullscreen: bool
     devices: str
-    db_dir: str = "visiomode_data/db"
 
-    def __init__(self, path=CONFIG_PATH):
-        """Initialises Config with a path to a configuration file.
+    _instance = None
 
-        If a valid configuration file exists, the program assumes it is NOT in debug mode, unless the config file specifies otherwise. If no config file exists, the program assumes it IS in debug mode.
+    def __new__(
+        cls,
+        config_path: typing.Optional[str] = None,
+    ) -> "Config":
+        """Initialise Config singleton with a path to a configuration file.
+
+        If a valid configuration file exists, the program assumes it is NOT in debug
+        mode, unless the config file specifies otherwise. If no config file exists, the
+        program assumes it IS in debug mode.
 
         Args:
-            path: Path to config JSON, defaults to DEFAULT_PATH. Only used if it exists.
+            config_path (str, optional): Path to config JSON, defaults to
+                                         DEFAULT_CONFIG_PATH. Only used if it exists.
         """
-        if os.path.exists(path):
-            logging.info(f"Loading config from {path}")
-            self._load_config(path)
-        else:
-            logging.warning("No config file found, using defaults")
-            self._set_defaults()
-            self.save()
+        if cls._instance is None:
+            cls._instance = super(Config, cls).__new__(cls)
 
-        os.makedirs(self.data_dir, exist_ok=True)
-        os.makedirs(self.cache_dir, exist_ok=True)
-        os.makedirs(self.db_dir, exist_ok=True)
+            if config_path is None:
+                config_path = DEFAULT_CONFIG_PATH
 
-    def save(self, path=CONFIG_PATH):
+            if os.path.exists(config_path):
+                logging.info(f"Loading config from {config_path}")
+                cls._load_config(config_path)
+            else:
+                logging.warning("No config file found, using defaults")
+                cls._set_defaults()
+                cls.save()
+
+            os.makedirs(cls.data_dir, exist_ok=True)
+            os.makedirs(cls.cache_dir, exist_ok=True)
+            os.makedirs(cls.db_dir, exist_ok=True)
+
+        return cls._instance
+
+    @classmethod
+    def save(cls, path: typing.Optional[str] = None):
+        if path is None:
+            path = DEFAULT_CONFIG_PATH
+
         with open(path, "w") as f:
-            json.dump(self.__dict__, f, indent=4)
+            json.dump(cls.to_dict(), f, indent=4)
 
-    def to_dict(self):
-        return self.__dict__
+    @classmethod
+    def to_dict(cls):
+        config = {}
+        for key in DEFAULT_CONFIG.keys():
+            try:
+                config[key] = getattr(cls, key)
+            except AttributeError:
+                logging.warning(f"Config is malformed, it does not have attribute '{key}'. Using default value.")
+                config[key] = DEFAULT_CONFIG[key]
+        return config
 
-    def _load_config(self, path):
+    @classmethod
+    def _load_config(cls, config_path: str):
         """Loads config from a JSON file.
 
         Args:
-            path: Path to config JSON file.
+            config_path: Path to config JSON file.
         """
-        with open(path, "r") as f:
+        with open(config_path, "r") as f:
             config = json.load(f)
 
         for key, value in config.items():
             if key in DEFAULT_CONFIG:
-                setattr(self, key, value)
+                setattr(cls, key, value)
             else:
                 logging.warning("Unknown config key: {}".format(key))
 
-    def _set_defaults(self):
+    @classmethod
+    def _set_defaults(cls):
         """Sets config to default values."""
         for key, value in DEFAULT_CONFIG.items():
-            setattr(self, key, value)
+            setattr(cls, key, value)
 
 
 def clear_cache():
     """Clears the cache directory."""
-    config = Config()
-    shutil.rmtree(config.cache_dir, ignore_errors=True)
-    os.makedirs(config.cache_dir, exist_ok=True)
+    shutil.rmtree(Config.cache_dir, ignore_errors=True)
+    os.makedirs(Config.cache_dir, exist_ok=True)
 
 
 def clear_db():
     """Clears the database directory."""
-    config = Config()
-    shutil.rmtree(config.db_dir, ignore_errors=True)
-    os.makedirs(config.db_dir, exist_ok=True)
+    shutil.rmtree(Config.db_dir, ignore_errors=True)
+    os.makedirs(Config.db_dir, exist_ok=True)
 
 
 def clear_data():
     """Clears the data directory."""
-    config = Config()
-    shutil.rmtree(config.data_dir, ignore_errors=True)
+    shutil.rmtree(Config.data_dir, ignore_errors=True)
 
     # Create new config file with default settings
+    Config._instance = None
     new_config = Config()
     new_config.save()
