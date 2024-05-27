c72da2f4162855106d431e954ac56077de85f753
Olivier Delree <olivierdelree@protonmail.com>
2024-05-27 17:29:17 +0100

    update copyright

--- src/visiomode/core.py
+++ src/visiomode/core.py
@@ -2,6 +2,7 @@
 
 #  This file is part of visiomode.
 #  Copyright (c) 2020 Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
+#  Copyright (c) 2024 Olivier Delree <odelree@ed.ac.uk>
 #  Distributed under the terms of the MIT Licence.
 
 import os
