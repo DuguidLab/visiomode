e345c5f87240813d709292bb58b99343233625c8
Olivier Delree <olivierdelree@protonmail.com>
2024-06-05 13:30:17 +0100

    add modal dialogue with session summary when clicking on session history
    It is now possible to click on a row in the history table and get a
    summary of the session information. The Javascript implementation makes
    use of the new GET API and only fetches the session of interest.
    If for some reason, the session file is no longer available when trying
    to populate the modal elements (e.g., the file has been removed but the
    page has not been refreshed since), empty strings are used instead.
    Similarly, if some fields (e.g., experimenter name) are not present in
    the file, the elements are populated with empty strings.

--- src/visiomode/webpanel/static/js/history.js
+++ src/visiomode/webpanel/static/js/history.js
@@ -17,6 +17,7 @@ fetch("/api/history")
         sessions.forEach(session => {
             console.log(session.animal_id)
             let row = sessionsTableData.insertRow();
+            row.style.cssText = "cursor: pointer;";
             let date = row.insertCell(0);
             date.innerHTML = session.date;
             let animal_id = row.insertCell(1);
@@ -37,5 +38,28 @@ fetch("/api/history")
                     <a class="dropdown-item" href="/api/download/nwb/${session.fname}">NWB</a>
                 </div>
             `;
+            let session_id = row.insertCell(-1);
+            session_id.innerHTML = session.session_id;
+            row.cells[4].classList.add("d-none");  // Don't display this column
         });
     });
+
+let table = document.getElementById("sessionsTableData");
+
+table.onclick = async function (event) {
+    let session_id = event.target.closest("tr").cells[event.target.parentNode.cells.length - 1].innerHTML;
+    let session = await fetch(`/api/history?session_id=${session_id}`)
+        .then((response) => response.json())
+        .then((data) => data.session);
+
+    $("#viewSession").modal();
+
+    document.getElementById("animal-id").value = session?.animal_id ?? "";
+    document.getElementById("duration").value = session?.duration ?? "";
+    document.getElementById("experiment-id").value = session?.experiment ?? "";
+    document.getElementById("experimenter-name").value = session?.experimenter_name ?? "";
+    document.getElementById("protocol").value = session?.protocol ?? "";
+    document.getElementById("timestamp").value = session?.timestamp ?? "";
+    document.getElementById("trial-count").value = session?.trials?.length ?? "";
+    document.getElementById("notes").value = session?.notes ?? "";
+}
