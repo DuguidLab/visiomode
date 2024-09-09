f71caa8737ffad4170ae62be6e5d399492805120
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2024-09-09 16:14:12 +0100

    add fixtures for webapp test client

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ from typing import Generator
 
 import pygame
 
+import visiomode.webpanel as webpanel
+
 
 @pytest.fixture(scope="module")
 def pygame_init() -> Generator[None, None, None]:
@@ -12,3 +14,29 @@ def pygame_init() -> Generator[None, None, None]:
     pygame.init()
     yield
     pygame.quit()
+
+
+@pytest.fixture()
+def webapp():
+    app = webpanel.create_app()
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(webapp):
+    return webapp.test_client()
+
+
+@pytest.fixture()
+def runner(webapp):
+    return webapp.test_cli_runner()
