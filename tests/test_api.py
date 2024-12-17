690f5557fa64a91df5d43a7168fdc6db0d462f36
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2024-12-17 17:20:50 +0000

    request stimuli and task tests by identifier

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,7 @@ import pytest
 
 import json
 from visiomode import stimuli
+from visiomode import tasks
 from visiomode.webpanel import api
 
 
@@ -33,18 +34,24 @@ def test_session_api_post(client, test_action_q):
 def test_stimulus_api_get(client):
     teststimuli = list(stimuli.Stimulus.get_children())
     for stimulus in teststimuli:
-        response = client.get(f"/api/stimulus-form/{stimulus}")
+        response = client.get(f"/api/stimulus-form/{stimulus.get_identifier()}")
         assert response.status_code == 200
     invalid_response = client.get("/api/stimulus-form/invalid")
     assert b"No Additional Options" in invalid_response.data
 
 
 def test_task_api_get(client):
-    ...
+    testtasks = list(tasks.Task.get_children())
+    for task in testtasks:
+        response = client.get(f"/api/task-form/{task.get_identifier()}")
+        assert response.status_code == 200
+    invalid_response = client.get("/api/task-form/invalid")
+    assert b"No Additional Options" in invalid_response.data
 
 
 def test_hostname_api_get(client):
-    ...
+    response = client.get(f"/api/hostname")
+    assert response.status_code == 200
 
 
 def test_history_api_get(client):
