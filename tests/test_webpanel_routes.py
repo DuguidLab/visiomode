7c04a611c9be37a3d7544700bd82acd18368afdb
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2025-01-07 12:43:06 +0000

    add skeleton for webpanel route tests

---
+++ tests/test_webpanel_routes.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+def test_index(client):
+    ...
+
+
+def test_history(client):
+    ...
+
+
+def test_settings(client):
+    ...
+
+
+def test_settings_animals(client):
+    ...
+
+
+def test_settings_experimenters(client):
+    ...
+
+
+def test_help(client):
+    ...
+
+
+def test_about(client):
+    ...
+
+
+def test_invalid_route(client):
+    ...
