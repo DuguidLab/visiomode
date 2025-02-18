b265f93779f3aafcf5964858921ffab90b811a80
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2025-02-18 17:25:02 +0000

    use mouse button press event instead of touch

--- src/visiomode/devices/touchscreen.py
+++ src/visiomode/devices/touchscreen.py
@@ -16,16 +16,21 @@ class Touchscreen(devices.InputDevice):
         self.config = conf.Config()
 
     def get_response(self):
-        touch_event = pg.event.get(eventtype=pg.FINGERDOWN)
+        # touch_event = pg.event.get(eventtype=pg.FINGERDOWN)
+        touch_event = pg.event.get(eventtype=pg.MOUSEBUTTONDOWN)
         if touch_event:
             logging.debug(f"Touch event registered - {touch_event}")
             # Flatten list. This essentially throws out all touch events that happen at an epoch shorter than the FPS.
             touch_event = touch_event[0]
-            pos_x = touch_event.x * self.config.width
-            pos_y = touch_event.y * self.config.height
-            dist_x = touch_event.dx * self.config.width
-            dist_y = touch_event.dy * self.config.height
-            name = "left" if pos_x >= (self.config.width / 2) else "right"
+            # pos_x = touch_event.x * self.config.width
+            # pos_y = touch_event.y * self.config.height
+            # dist_x = touch_event.dx * self.config.width
+            # dist_y = touch_event.dy * self.config.height
+            pos_x = touch_event.pos[0]
+            pos_y = touch_event.pos[1]
+            dist_x = 0
+            dist_y = 0
+            name = "left" if pos_x <= (self.config.width / 2) else "right"
             return models.Response(
                 timestamp=datetime.datetime.now().isoformat(),
                 name=name,
